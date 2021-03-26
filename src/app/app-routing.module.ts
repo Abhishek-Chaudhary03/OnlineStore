ade55052482c7047e479ec9ccda9a8e353d77221
Abhishek-Chaudhary03 <abhishek21chaudhary@gmail.com>
2021-03-26 11:45:00 +0530

    Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './shared/aboutus/aboutus.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 const routes : Routes =[
   {path: '', redirectTo: '/aboutus', pathMatch:'full'},
   {path: 'aboutus', component: AboutusComponent},
+  {path: 'header', component: HeaderComponent},
+  {path: 'footer', component: FooterComponent},
   {path: '**', redirectTo: '/aboutus', pathMatch:'full'},
 ]
 
